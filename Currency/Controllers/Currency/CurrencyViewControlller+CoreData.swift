862b9198cbabb0b5c684d766e58f9b0dc116e9c0
Maruf895 <mohd.maruf@nagarro.com>
2022-04-07 12:29:28 +0530

    feature: CoreData: save currencies functionality implemented

---
+++ Currency/Controllers/Currency/CurrencyViewControlller+CoreData.swift
@@ -0,0 +1,73 @@
+//
+//  CurrencyViewControlller+CoreData.swift
+//  Currency
+//
+//  Created by Mohd Maruf on 07/04/22.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+extension CurrencyViewController {
+    
+    func saveCurrenciesToCoreData() {
+        if !isCurrenciesAlreadySaved() {
+            let currencySearched = NSEntityDescription.insertNewObject(
+                forEntityName: "\(CurrencySearched.self)",
+                into: getContext()
+            ) as! CurrencySearched
+            
+            currencySearched.fromCurrency = viewModel.currencyModel.fromCurrency
+            currencySearched.toCurrency = viewModel.currencyModel.toCurrency
+            currencySearched.date = Date()
+            
+            do {
+                try getContext().save()
+            } catch let err {
+                alert(message: err.localizedDescription)
+            }
+        }
+    }
+    
+    func isCurrenciesAlreadySaved() -> Bool {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "\(CurrencySearched.self)")
+        request.returnsObjectsAsFaults = false
+        
+        request.predicate = getDatePredicate()
+        do {
+            let currencySearched = try getContext().fetch(request) as? [CurrencySearched]
+            return !(currencySearched?.isEmpty ?? true)
+        } catch let err {
+            alert(message: err.localizedDescription)
+        }
+        return false
+    }
+    
+    private func getContext() -> NSManagedObjectContext {
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        
+        return delegate.persistentContainer.viewContext
+    }
+    
+    private func getDatePredicate() -> NSPredicate {
+        // Get the current calendar with local time zone
+        var calendar = Calendar.current
+        calendar.timeZone = NSTimeZone.local
+        
+        // Get today's beginning & end
+        let dateFrom = calendar.startOfDay(for: Date()) // eg. 2016-10-10 00:00:00
+        let dateTo = calendar.date(byAdding: .day, value: 1, to: dateFrom)!
+        
+        // Set predicate as date being today's date
+        let fromPredicate = NSPredicate(format: "date >= %@", dateFrom as NSDate)
+        let toPredicate = NSPredicate(format: "date < %@",  dateTo as NSDate)
+        let fromCurrencyPredicate = NSPredicate(format: "fromCurrency == %@",  viewModel.currencyModel.fromCurrency ?? "")
+        let toCurrencyPredicate = NSPredicate(format: "toCurrency == %@",  viewModel.currencyModel.toCurrency ?? "")
+        let datePredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
+            fromPredicate, toPredicate, fromCurrencyPredicate, toCurrencyPredicate
+        ])
+        
+        return datePredicate
+    }
+}
--- Currency/Controllers/DetailViewController/DetailViewController.swift
+++ Currency/Controllers/DetailViewController/DetailViewController.swift
@@ -9,4 +9,5 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
 }
